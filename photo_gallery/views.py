3b0cc70b9a5aa2a2c500e31ccb913aa34e4a7416
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-06-28 18:13:23 +0300

    defined the photo views and profile

--- photo_gallery/views.py
+++ photo_gallery/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Photo, Tag, UserProfile
 
-# Create your views here.
+def home(request):
+    tag = request.GET.get('tag')
+    photos = Photo.objects.filter(tags__name=tag) if tag else Photo.objects.all()
+    tags = Tag.objects.all()
+    return render(request, 'home.html', {'photos': photos, 'tags': tags})
+
+def photo_detail(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    return render(request, 'photo_detail.html', {'photo': photo})
+
+@login_required
+def profile(request):
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
+    return render(request, 'profile.html', {'profile': profile})
