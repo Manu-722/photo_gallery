75a0e730a92024ba0ceaba78c8eeb7568f00f003
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-06-29 14:25:21 +0300

    created the userprofile,tag and photo functions

--- photo_gallery/models.py
+++ photo_gallery/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='photos/')
+    tags = models.ManyToManyField(Tag, blank=True)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='liked_photos', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True)
+
+    def __str__(self):
+        return self.user.username
