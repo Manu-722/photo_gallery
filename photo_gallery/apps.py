07684cc3d13f39efc98774cb7488ddcc7d88e4b7
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-06-29 14:28:55 +0300

    imported the signals file and created a class for the gallery

--- photo_gallery/apps.py
+++ photo_gallery/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class PhotoGalleryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'photo_gallery'
+def ready(self):
+        import photo_gallery.signals
